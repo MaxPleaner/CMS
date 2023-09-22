5951927ae8abdcbb7d53838c75f0dbf545106677
max pleaner <max.pleaner@hover.to>
2023-09-22 02:27:09 -0700

    finish most of the todos

--- app.rb
+++ app.rb
@@ -4,6 +4,8 @@ require 'slim'
 require "sinatra/reloader" if development?
 require 'sinatra/flash'
 
+# TODO: support layout functionality
+
 enable :sessions
 set :public_folder, File.dirname(__FILE__) + "/grapesjs"
 SITES_DIR = "./sites"
@@ -16,7 +18,7 @@ SITES_DIR = "./sites"
 get '/sites/:site/pages/:page/editor' do
   @site = alphanumeric(params[:site])
   @page = alphanumeric(params[:page])
-  @published = published?(@site, @page)
+  @activated = activated?(@site, @page)
 
   erb File.read(File.join(settings.public_folder, "demo.html.erb"))
 end
@@ -26,21 +28,14 @@ end
 # ---------------------------------------------
 
 post '/sites/:site/pages/:page/editor/store' do
-
-  @site = alphanumeric(params[:site])
-  @page = alphanumeric(params[:page])
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
-
-  pages_path = File.join(site_path, @page)
-  return 404 if same_path?(pages_path, site_path)
+  return 404 unless load_site_and_page(params)
 
   request_body = JSON.parse(request.body.read)
 
   grapesjs_data = request_body.slice("data")
 
   # Write JSON data
-  storage_path = File.join(pages_path, "data.json")
+  storage_path = File.join(@page_path, "data-saved.json")
   File.open(storage_path, "w") { |f| f.write grapesjs_data.to_json }
 
   # Write HTML
@@ -48,7 +43,7 @@ post '/sites/:site/pages/:page/editor/store' do
   body, css = html_data[0].values_at("html", "css")
   html = build_html(body, css)
 
-  html_path = File.join(pages_path, "index.html")
+  html_path = File.join(@page_path, "index-saved.html")
   File.open(html_path, "w") { |f| f.write html }
 
   return 200
@@ -59,16 +54,9 @@ end
 # ---------------------------------------------
 
 get '/sites/:site/pages/:page/editor/load' do
-  @site = alphanumeric(params[:site])
-  @page = alphanumeric(params[:page])
+  return 404 unless load_site_and_page(params)
 
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
-
-  pages_path = File.join(site_path, @page)
-  return 404 if same_path?(pages_path, site_path)
-
-  storage_path = File.join(pages_path, "data.json")
+  storage_path = File.join(@page_path, "data-saved.json")
   send_file storage_path
 end
 
@@ -76,24 +64,59 @@ end
 # Publish a page
 # ---------------------------------------------
 
-get '/sites/:site/page/:page/publish' do
-  # TODO
+post '/sites/:site/page/:page/publish' do
+  return 404 unless load_site_and_page(params)
+
+  storage_path = File.join(@page_path, "data-saved.json")
+  html_path = File.join(@page_path, "index-saved.html")
+  return 404 unless [storage_path, html_path].all? { |path| File.exists?(path) }
+
+  FileUtils.cp(storage_path, File.join(@page_path, "data.json"))
+  FileUtils.cp(html_path, File.join(@page_path, "index.html"))
+
+  return 200
 end
 
 # ---------------------------------------------
-# Unpublish a page
+# Deactivate a page
 # ---------------------------------------------
 
-get '/sites/:site/page/:page/unpublish' do
-  # TODO
+post '/sites/:site/page/:page/deactivate' do
+  return 404 unless load_site_and_page(params)
+
+  FileUtils.touch(File.join(@page_path, "deactivated"))
+
+  redirect "/sites/#{@site}"
+end
+
+# ---------------------------------------------
+# Reactivate a page
+# ---------------------------------------------
+
+post '/sites/:site/page/:page/reactivate' do
+  return 404 unless load_site_and_page(params)
+
+  file_path = File.join(@page_path, "deactivated")
+  FileUtils.rm(file_path) if File.exists?(file_path)
+
+  redirect "/sites/#{@site}"
 end
 
 # ---------------------------------------------
 # Revert changes since last publish
 # ---------------------------------------------
 
-get '/sites/:site/page/:page/revert' do
-  # TODO
+post '/sites/:site/page/:page/revert' do
+  return 404 unless load_site_and_page(params)
+
+  storage_path = File.join(@page_path, "data.json")
+  html_path = File.join(@page_path, "index.html")
+  return 404 unless [storage_path, html_path].all? { |path| File.exists?(path) }
+
+  FileUtils.cp(storage_path, File.join(@page_path, "data-saved.json"))
+  FileUtils.cp(html_path, File.join(@page_path, "index-saved.html"))
+
+  redirect "/sites/#{@site}/pages/#{@page}/editor"
 end
 
 # ---------------------------------------------
@@ -109,24 +132,52 @@ end
 # Clone site
 # ---------------------------------------------
 
-get '/sites/:site/clone' do
-  # TODO
+post '/sites/:site/clone' do
+  return 404 unless load_site(params)
+
+  new_site = alphanumeric(params[:new_name])
+  new_path = File.join(SITES_DIR, new_site)
+
+  if File.exists?(new_path)
+    flash[:message] = "New site name is already taken."
+    redirect "/"
+  end
+
+  FileUtils.cp_r(@site_path, new_path)
+
+  redirect "/sites/#{new_site}"
 end
 
 # ---------------------------------------------
 # Clone page
 # ---------------------------------------------
 
-get '/sites/:site/pages/#{page}/clone' do
-  # TODO
+post '/sites/:site/pages/:page/clone' do
+  return 404 unless load_site_and_page(params)
+
+  new_page = alphanumeric(params[:new_name])
+  new_path = File.join(@site_path, new_page)
+
+  if File.exists?(new_path)
+    flash[:message] = "New page name is already taken."
+    redirect "/sites/#{@site}"
+  end
+
+  FileUtils.cp_r(@page_path, new_path)
+
+  redirect "/sites/#{@site}"
 end
 
 # ---------------------------------------------
 # Delete site
 # ---------------------------------------------
 
-get '/sites/:site/delete' do
-  # TODO
+post '/sites/:site/delete' do
+  return 404 unless load_site(params)
+
+  FileUtils.rm_rf(@site_path)
+
+  redirect "/"
 end
 
 # ---------------------------------------------
@@ -134,11 +185,9 @@ end
 # ---------------------------------------------
 
 get "/sites/:site" do
-  @site = alphanumeric(params[:site])
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
+  return 404 unless load_site(params)
 
-  @pages = subfolders(site_path) || []
+  @pages = subfolders(@site_path) || []
 
   slim :pages_manager
 end
@@ -148,16 +197,14 @@ end
 # ---------------------------------------------
 
 post "/sites" do
-  @site = alphanumeric(params[:site])
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
+  return 404 unless load_site(params)
 
-  if File.exists?(site_path)
+  if File.exists?(@site_path)
     flash[:message] = "Site already exists"
     redirect "/"
   end
 
-  FileUtils.mkdir(site_path)
+  FileUtils.mkdir(@site_path)
 
   redirect "/sites/#{@site}"
 end
@@ -168,20 +215,14 @@ end
 # ---------------------------------------------
 
 post "/sites/:site/pages" do
-  @site = alphanumeric(params[:site])
-  @page = alphanumeric(params[:page])
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
+  return 404 unless load_site_and_page(params)
 
-  pages_path = File.join(site_path, @page)
-  return 404 if same_path?(pages_path, site_path)
-
-  if File.exists?(pages_path)
+  if File.exists?(@page_path)
     flash[:message] = "Page name is already taken"
     redirect "/sites/#{@site}"
   end
 
-  FileUtils.mkdir(pages_path)
+  FileUtils.mkdir(@page_path)
 
   redirect "/sites/#{@site}"
 end
@@ -191,20 +232,14 @@ end
 # ---------------------------------------------
 
 post "/sites/:site/pages/:page/delete" do
-  @site = alphanumeric(params[:site])
-  @page = alphanumeric(params[:page])
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
+  return 404 unless load_site_and_page(params)
 
-  pages_path = File.join(site_path, @page)
-  return 404 if same_path?(pages_path, site_path)
-
-  unless File.exists?(pages_path)
+  unless File.exists?(@page_path)
     flash[:message] = "Page doesnt exist"
     redirect "/sites/#{@site}"
   end
 
-  FileUtils.rm_rf(pages_path)
+  FileUtils.rm_rf(@page_path)
 
   redirect "/sites/#{@site}"
 end
@@ -216,15 +251,11 @@ end
 # ---------------------------------------------
 
 get '/:site/:page' do
-  @site = alphanumeric(params[:site])
-  @page = alphanumeric(params[:page])
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
+  return 404 unless load_site_and_page(params)
 
-  pages_path = File.join(site_path, @page)
-  return 404 if same_path?(pages_path, site_path)
+  return 404 unless activated?(@site, @page)
 
-  html_file = File.join(pages_path, "index.html")
+  html_file = File.join(@page_path, "index.html")
   return 404 unless File.exists?(html_file)
 
   send_file html_file
@@ -235,15 +266,9 @@ end
 # ---------------------------------------------
 
 get "/:site" do
-  @site = alphanumeric(params[:site])
-  @page = "index"
-  site_path = File.join(SITES_DIR, @site)
-  return 404 if same_path?(site_path, SITES_DIR)
+  return 404 unless load_site(params)
 
-  pages_path = File.join(site_path, @page)
-  return 404 if same_path?(pages_path, site_path)
-
-  html_file = File.join(pages_path, "index.html")
+  html_file = File.join(@site_path, "index", "index.html")
   return 404 unless File.exists?(html_file)
 
   send_file html_file
@@ -260,8 +285,39 @@ end
 
 helpers do
   def published?(site, page)
-    # TODO
+    site_path = File.join(SITES_DIR, site)
+    pages_path = File.join(site_path, page)
+    html_path = File.join(pages_path, "index.html")
+
+    File.exists?(html_path)
   end
+
+  def activated?(site, page)
+    site_path = File.join(SITES_DIR, site)
+    pages_path = File.join(site_path, page)
+
+    !File.exists?(File.join(pages_path, "deactivated"))
+  end
+end
+
+def load_site(params)
+  @site = alphanumeric(params[:site])
+  @site_path = File.join(SITES_DIR, @site)
+  return false if same_path?(@site_path, SITES_DIR)
+
+  true
+end
+
+def load_site_and_page(params)
+  return false unless load_site(params)
+
+  @page = alphanumeric(params[:page])
+  return nil unless @page
+
+  @page_path = File.join(@site_path, @page)
+  return false if same_path?(@page_path, @site_path)
+
+  true
 end
 
 def same_path?(a, b)
@@ -273,6 +329,7 @@ def safe_dir?(test_folder, parent_folder)
 end
 
 def alphanumeric(string)
+  return unless string
   string.gsub(/[^a-zA-Z0-9_-]/, '')
 end
 
